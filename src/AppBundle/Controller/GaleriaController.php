f397a77c0074dd41932bbdfe0409a00f0c82ec97
Anna Zaremba <aniazarem@gmail.com>
2017-08-10 12:24:42 +0200

    #10 - wyświetlanie zdjęć z flickr API

--- src/AppBundle/Controller/GaleriaController.php
+++ src/AppBundle/Controller/GaleriaController.php
@@ -14,6 +14,42 @@ class GaleriaController extends Controller
      */
     public function galeriaAction()
     {
-        return [];
+        $params = array(
+            'api_key' => '8da87a1824d3b7ad151e789438c6ed10',
+            'method' => 'flickr.photos.getInfo',
+            'photo_id' => '36308778775',
+            'format' => 'php_serial',
+        );
+
+        $encoded_params = array();
+
+        foreach ($params as $k => $v) {
+
+            $encoded_params[] = urlencode($k) . '=' . urlencode($v);
+        }
+
+
+        $url = "https://api.flickr.com/services/rest/?" . implode('&', $encoded_params);
+
+        echo $url;
+
+        $rsp = file_get_contents($url);
+
+        $rsp_obj = unserialize($rsp);
+
+
+        if ($rsp_obj['stat'] == 'ok') {
+
+            $photo_title = $rsp_obj['photo']['title']['_content'];
+
+            echo "Title is $photo_title!";
+        } else {
+
+            echo "Call failed!";
+        }
+
+        return [
+            'title' => $photo_title,
+        ];
     }
 }
